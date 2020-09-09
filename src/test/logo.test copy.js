13768b1b41ecb4a5d84261d40e24ff172a094f7f
Matt <matthewjmcivor@gmail.com>
2020-09-08 19:09:37 -0500

    Attempt to fix Travis

--- src/test/logo.test copy.js
+++ src/test/logo.test copy.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import Logo from "../components/logo/Logo.jsx";
+import Logo from "../components/logo/logo.jsx";
 
 test("renders learn react link", () => {
   const { getByText } = render(<Logo />);
